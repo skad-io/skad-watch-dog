b77e7a7939fe60542c2df35f031393bd0f5b12e8
scottclee <scott.john.clee@gmail.com>
2016-03-22 20:50:34 +0000

    Update configurehotspot.sh

--- configurehotspot.sh
+++ configurehotspot.sh
@@ -10,3 +10,16 @@ cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.orig
 sed -e '/option domain-name "example.org";/ s/^#*/#/' -i /etc/dhcp/dhcpd.conf
 sed -e '/option domain-name-servers ns1.example.org, ns2.example.org;/ s/^#*/#/' -i /etc/dhcp/dhcpd.conf
 
+echo '# If this DHCP server is the official DHCP server for the local' >> /etc/dhcp/dhcpd/conf
+echo '# network, the authoritative directive should be uncommented.' >> /etc/dhcp/dhcpd/conf
+echo 'authoritative;' >> /etc/dhcp/dhcpd/conf
+echo '' >> /etc/dhcp/dhcpd/conf
+echo 'subnet 192.168.42.0 netmask 255.255.255.0 {' >> /etc/dhcp/dhcpd/conf
+echo '	range 192.168.42.10 192.168.42.50;' >> /etc/dhcp/dhcpd/conf
+echo '	option broadcast-address 192.168.42.255;' >> /etc/dhcp/dhcpd/conf
+echo '	option routers 192.168.42.1;' >> /etc/dhcp/dhcpd/conf
+echo '	default-lease-time 600;' >> /etc/dhcp/dhcpd/conf
+echo '	max-lease-time 7200;' >> /etc/dhcp/dhcpd/conf
+echo '	option domain-name "local";' >> /etc/dhcp/dhcpd/conf
+echo '	option domain-name-servers 8.8.8.8, 8.8.4.4;' >> /etc/dhcp/dhcpd/conf
+echo '}' >> /etc/dhcp/dhcpd/conf
