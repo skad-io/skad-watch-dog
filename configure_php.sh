e7b82ffb1c4cc5fe316e54ad719553a8da47025c
scottclee <scott.john.clee@gmail.com>
2016-03-23 20:39:45 +0000

    Create configure_php.sh

---
+++ configure_php.sh
@@ -0,0 +1,24 @@
+# TODO: Place all these into a useful commands file
+# Replace a string in a file:
+# sed -i 's/x/y/g' /etc/nginx/sites-available/default
+
+# Tutorial:
+# https://www.howtoforge.com/tutorial/installing-nginx-with-php-fpm-and-mariadb-lemp-on-debian-jessie/
+
+apt-get install php5-fpm
+
+# cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
+# It says to change keepalive_timeout to 2 seconds but not sure why so not doing it for now
+
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
+
+echo 'These commands dont seem to be working'
+
+sed -i 's/#location ~ \.php$ {/location ~ \.php$ {/g' /etc/nginx/sites-available/default
+sed -i 's/#       include snippets\/fastcgi-php.conf;/       include snippets\/fastcgi-php.conf;/g' /etc/nginx/sites-available/default
+sed -i 's/#       fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/       fastcgi_pass unix:\/var\/run\/php5-fpm.sock;/g' /etc/nginx/sites-available/default
+sed -i 's/#location ~ \/\\.ht {/location ~ \/\\.ht {/g' /etc/nginx/sites-available/default
+sed -i 's/#       deny all;/       deny all;/g' /etc/nginx/sites-available/default
+
+systemctl reload nginx.service
+
