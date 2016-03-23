a735e618433d4ed6c364e71d144c15f42f7cdaf4
scottclee <scott.john.clee@gmail.com>
2016-03-23 18:31:01 +0000

    Create configurehotspot_2.sh

---
+++ configurehotspot_2.sh
@@ -0,0 +1,12 @@
+
+# Command to comment out a line in a file
+# sed -e '/COMMENTMEOUT/ s/^#*/#/' -i FILENAME
+
+################################################################
+# The following instructions come from this page:
+# https://learn.adafruit.com/setting-up-a-raspberry-pi-as-a-wifi-access-point/install-software
+
+apt-get -y install vim
+apt-get -y install hostapd isc-dhcp-server
+
+# Set up DHCP server
