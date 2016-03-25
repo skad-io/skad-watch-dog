b9ef41683214e2070b27a1048d9fff95243d93c4
scottclee <scott.john.clee@gmail.com>
2016-03-25 15:22:14 +0000

    Update configure_php.sh

--- configure_php.sh
+++ configure_php.sh
@@ -1,7 +1,7 @@
 # Kudos to this tutorial:
 # https://www.howtoforge.com/tutorial/installing-nginx-with-php-fpm-and-mariadb-lemp-on-debian-jessie/
 
-apt-get install php5-fpm
+apt-get -y install php5-fpm
 
 # cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 # It says to change keepalive_timeout to 2 seconds but not sure why so not doing it for now
