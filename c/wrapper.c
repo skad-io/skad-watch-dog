2ff2bbf833bcbd4cfc038bd2bf3ebd06bfafb20f
scottclee <scott.john.clee@gmail.com>
2016-03-25 16:53:52 +0000

    Create wrapper.c

---
+++ c/wrapper.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main (int argc, char *argv[])
+{
+   setuid (0);
+
+   /* WARNING: Only use an absolute path to the script to execute,
+    *          a malicious user might fool the binary and execute
+    *          arbitary commands if not.
+    * */
+
+   char* command = "/bin/sh /home/pi/php_shell.sh";
+
+   if (argc > 1) {
+         char* tmpString = malloc(strlen(command)+1+strlen(argv[0])+1);
+         strcpy(tmpString, command);
+         strcat(tmpString, " ");
+         strcat(tmpString, argv[1]);
+         command = tmpString;
+   }
+
+   system(command);
+
+   return 0;
+}
