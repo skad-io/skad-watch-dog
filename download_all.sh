afb46fc65d8b0d7ce3a0e116d64927badcfe16b2
scottclee <scott.john.clee@gmail.com>
2016-03-25 15:36:49 +0000

    Update download_all.sh

--- download_all.sh
+++ download_all.sh
@@ -1,5 +1,4 @@
 wget https://github.com/scottclee/SKAD/archive/wifi-hotspot-scripts.zip
 unzip wifi-hotspot-scripts.zip
 rm wifi-hotspot-scripts.zip
-cd SKAD-wifi-hotspot-scripts
-chmod +x *.sh
+chmod +x SKAD-wifi-hotspot-scripts/*.sh
