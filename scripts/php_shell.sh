f075bf76025fc38186bc07eb7e4aebaabe1532ea
Scott Clee <scott@scott-laptop.home>
2016-03-27 13:16:35 +0100

    Had incorrect hardcode path

--- scripts/php_shell.sh
+++ scripts/php_shell.sh
@@ -2,4 +2,4 @@
 
 /bin/echo "Parameters: ${1}"
 echo "<BR>"
-/usr/bin/perl -I/home/pi/SKAD-wifi-hotspot-scripts/perl /home/pi/SKAD-wifi-hotspot-scripts/perl/getNginxServerConfigs.pl
+/usr/bin/perl -I/home/pi/SKAD/perl /home/pi/SKAD/perl/getNginxServerConfigs.pl
