19e488c43a72d3919d6d80b0cd52052be8d2c526
Scott Clee <scott@scott-laptop.home>
2016-03-27 15:12:04 +0100

    Relative path for symbolic link wasn't working so added absolute path

--- scripts/configure_webservices.sh
+++ scripts/configure_webservices.sh
@@ -1,5 +1,5 @@
 mv /var/www/html /var/www/html.orig
-ln -s ../html /var/www/html
+ln -s /home/pi/SKAD/html /var/www/html
 gcc ../c/wrapper.c -o /home/pi/php_root
 chown root /home/pi/php_root
 chmod u=rwx,go=xr,+s /home/pi/php_root
