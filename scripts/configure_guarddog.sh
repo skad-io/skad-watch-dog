5ae8f7a23a603471479fe36a2d4be5f570700d56
Scott Clee <scott@scott-laptop.home>
2016-03-27 19:39:53 +0100

    Should have headless wifi setup working properly now and with a copy of the guarddog home page for its setup

--- scripts/configure_guarddog.sh
+++ scripts/configure_guarddog.sh
@@ -3,6 +3,7 @@
 ./configure_php.sh
 ./configure_cpanJson.sh
 ./configure_webservices.sh
+./configure_headlesswifisetup.sh
 ./configure_expandRootFs.sh
 ./configure_hostname.sh guarddog
 echo "Please reboot me now"
