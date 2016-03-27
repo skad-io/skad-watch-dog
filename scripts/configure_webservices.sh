f47a4cdce6dd32fb9f01aef45675870e6ab495e5
Scott Clee <scott@scott-laptop.home>
2016-03-27 15:09:24 +0100

    Renamed php folder to html and now creates a symbolic link to it in /var/www on install

--- scripts/configure_webservices.sh
+++ scripts/configure_webservices.sh
@@ -1,5 +1,5 @@
-cp ../php/*.php /var/www/html/
+mv /var/www/html /var/www/html.orig
+ln -s ../html /var/www/html
 gcc ../c/wrapper.c -o /home/pi/php_root
 chown root /home/pi/php_root
 chmod u=rwx,go=xr,+s /home/pi/php_root
-cp php_shell.sh /home/pi/
