a55be9f20a32300ec6a4fa847607e4f9655de146
Scott Clee <scott@scott-laptop.home>
2016-03-27 18:34:49 +0100

    Was pointing to wrong directories for symbolic links

--- scripts/configure_webservices.sh
+++ scripts/configure_webservices.sh
@@ -1,6 +1,6 @@
 mv /var/www/html /var/www/html.orig
-ln -s /home/pi/SKAD/html /var/www/html/guarddog/
-ln -s /home/pi/SKAD/php /home/pi/SKAD/html/php
+ln -s /home/pi/SKAD/html/guarddog /var/www/html
+ln -s /home/pi/SKAD/php /var/www/html/php
 gcc ../c/wrapper.c -o /home/pi/php_root
 chown root /home/pi/php_root
 chmod u=rwx,go=xr,+s /home/pi/php_root
