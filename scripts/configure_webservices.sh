d53449bbdc13cf3358a0c8e141212181bab1e007
scottclee <scott.john.clee@gmail.com>
2016-03-28 12:44:32 +0100

    Update configure_webservices.sh

--- scripts/configure_webservices.sh
+++ scripts/configure_webservices.sh
@@ -1,6 +1,7 @@
 mv /var/www/html /var/www/html.orig
 ln -s /home/pi/SKAD/html/guarddog /var/www/html
-ln -s /home/pi/SKAD/php /var/www/html/php
+#I believe the symbolic link is now stored in git
+#ln -s /home/pi/SKAD/php /var/www/html/php
 gcc ../c/wrapper.c -o /home/pi/php_root
 chown root /home/pi/php_root
 chmod u=rwx,go=xr,+s /home/pi/php_root
