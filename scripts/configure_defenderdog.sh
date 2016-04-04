c88c97bce5d4cb3396bb5a52d58907bb28c06dc4
Scott Clee <scott@scott-laptop.home>
2016-04-04 23:24:59 +0100

    Changed order of lets encrypt install meaning it shouldn't prompt during install

--- scripts/configure_defenderdog.sh
+++ scripts/configure_defenderdog.sh
@@ -1,11 +1,11 @@
 ./install_packages.sh
 ./configure_core.sh
+./configure_letsencrypt.sh
 ./configure_nginx.sh
 ./configure_php.sh
 ./configure_cpanJson.sh
 ./configure_webservices.sh
 ./configure_headlesswifisetup.sh
-./configure_letsencrypt.sh
 ./configure_hostname.sh defenderdog
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
 cp /home/pi/SKAD/files/guarddog/default /etc/nginx/sites-available/
