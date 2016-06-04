f9fab1fe67fb979cf3efdebdb0b71aa27b6d9627
scottclee <scott@clee.pro>
2016-06-04 11:00:44 +0100

    Add use of branding variable to configure_reverseproxy.sh

--- scripts/configure_reverseproxy.sh
+++ scripts/configure_reverseproxy.sh
@@ -1,12 +1,13 @@
+source ./variables.sh
 # ./install_some_packages_early_temp_fix.sh
 ./configure_core.sh
-./configure_letsencrypt.sh
 ./configure_nginx.sh
 ./configure_php.sh
 ./configure_cpanJson.sh
 ./configure_webservices.sh
 ./configure_headlesswifisetup.sh
-./configure_hostname.sh defenderdog
+./configure_letsencrypt.sh
+./configure_hostname.sh $REVERSE_PROXY_BRANDING
 mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
-cp /home/pi/SKAD/files/guarddog/default /etc/nginx/sites-available/
+cp /home/pi/SKAD/files/guarddog.unsecure/default /etc/nginx/sites-available/
 echo "Please reboot me now"
