1729d85cb133bb0ff2470fdde32557a66b5ba972
scottclee <scott@clee.pro>
2016-06-04 11:52:27 +0100

    Fix directory error in letsencrypt script

--- scripts/configure_letsencrypt.sh
+++ scripts/configure_letsencrypt.sh
@@ -7,4 +7,4 @@ git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 # When executed from the web page the letsencrypt command actually runs under the www-data user and so sees is home directory as /var/www which means it does not find the letsencrypt install and tries to run it again. So creating a symbolic link to the one in the root directory
 /bin/ln -s /root/.local /var/www/.local
 
-cp /home/pi/SKAD/files/guarddog/cli.ini /etc/letsencrypt/ 
+cp /home/pi/SKAD/files/guarddog.unsecure/cli.ini /etc/letsencrypt/ 
