54ca0718e07b7e751ded3b293e926d4a023283f1
scottclee <scott@clee.pro>
2016-07-03 14:26:14 +0000

    Fix path to correct php.ini

--- scripts/configure_mongodb.sh
+++ scripts/configure_mongodb.sh
@@ -2,4 +2,4 @@ apt-get -y install mongodb-server
 apt-get -y install php-pear php5-dev
 # TODO Automate the following command as it prompts the user for response
 pecl install mongo
-echo "extension=mongo.so" >> /etc/php5/cli/php.ini
+echo "extension=mongo.so" >> /etc/php5/fpm/php.ini
